080f47409b86bd674a8998214cb5510640a6286f
liming <1084611818@qq.com>
2024-06-16 21:06:12 +0800

    完成cancel-service

--- service/wait_order_service.go
+++ service/wait_order_service.go
@@ -1,1 +1,60 @@
 package service
+
+import (
+	"encoding/json"
+	"io"
+)
+
+func (s *SvcImpl) ReqCreateNewOrder_WaitOrder(input *OrderVO) (*OrderResp, error) {
+	resp, err := s.cli.SendRequest("POST", s.BaseUrl+"/api/v1/waitorderservice/order", input)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result OrderResp
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (s *SvcImpl) ReqGetAllOrders_WaitOrder() (*OrderArrResp, error) {
+	resp, err := s.cli.SendRequest("GET", s.BaseUrl+"/api/v1/waitorderservice/orders", nil)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result OrderArrResp
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (s *SvcImpl) ReqGetWaitListOrders() (*OrderArrResp, error) {
+	resp, err := s.cli.SendRequest("GET", s.BaseUrl+"/api/v1/waitorderservice/waitlistorders", nil)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result OrderArrResp
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
