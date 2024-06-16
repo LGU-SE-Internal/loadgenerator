500a73b0e3eb05dd816120e80ca67b3470d29cca
liming <1084611818@qq.com>
2024-06-16 22:15:07 +0800

    完成execute-service

--- service/execute_service_test.go
+++ service/execute_service_test.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSvcImpl_ReqExecuteTicket(t *testing.T) {
+	cli, _ := GetAdminClient()
+	GetResp, _ := cli.ReqExecuteTicket("7c83f029-73ab-40e5-bb6c-a45dffaab06b")
+	fmt.Println(GetResp.Msg)
+}
+
+func TestSvcImpl_ReqCollectTicket(t *testing.T) {
+	cli, _ := GetAdminClient()
+	GetResp, _ := cli.ReqCollectTicket("7f30d9c9-bc07-4494-865e-cde5d8511b1d")
+	fmt.Println(GetResp.Msg)
+}
