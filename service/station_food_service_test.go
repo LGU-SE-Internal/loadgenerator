8fa5c1901ce775b6d9d57acac0fc4e8c80e09b59
lincyaw <814750204@qq.com>
2024-06-16 22:07:51 +0800

    add station food service test

--- service/station_food_service_test.go
+++ service/station_food_service_test.go
@@ -1,1 +1,35 @@
 package service
+
+import "testing"
+
+func TestSvcImpl_GetAllStationFood(t *testing.T) {
+	cli, _ := GetAdminClient()
+
+	resp, err := cli.GetAllStationFood()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp)
+
+	for _, stationFood := range resp.Data {
+		sf, err := cli.GetStationFoodByNames([]string{stationFood.StationName})
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(sf)
+
+		sf1, err := cli.GetStationFoodByName(stationFood.StationName)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(sf1)
+
+		sf2, err := cli.GetStationFoodById(stationFood.Id)
+		if err != nil {
+			t.Error(err)
+		}
+
+		t.Log(sf2)
+	}
+
+}
