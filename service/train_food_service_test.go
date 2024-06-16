e4112ebd931b8722ec7214123de8045a01977025
lincyaw <814750204@qq.com>
2024-06-16 22:31:12 +0800

    add train food service test

--- service/train_food_service_test.go
+++ service/train_food_service_test.go
@@ -1,1 +1,20 @@
 package service
+
+import "testing"
+
+func TestSvcImplGetAllTrainFood(t *testing.T) {
+	cli, _ := GetBasicClient()
+	resp, err := cli.GetAllTrainFood()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp)
+
+	for _, train := range resp.Data {
+		trainFood, err := cli.GetTrainFoodByTripId(train.TripId)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(trainFood)
+	}
+}
