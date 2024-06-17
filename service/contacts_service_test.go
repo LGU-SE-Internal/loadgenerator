5bc59ba0427d63f8de91efe3bfd239a784d42bea
lincyaw <814750204@qq.com>
2024-06-17 11:39:49 +0800

    add contact service test

--- service/contacts_service_test.go
+++ service/contacts_service_test.go
@@ -1,1 +1,65 @@
 package service
+
+import (
+	"github.com/go-faker/faker/v4"
+	"github.com/google/uuid"
+	"testing"
+)
+
+func TestSvcImpl_GetAllContacts(t *testing.T) {
+	cli, _ := GetBasicClient()
+	contacts, err := cli.GetAllContacts()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[get all]: ", contacts)
+
+	contactResp, err := cli.AddContact(&Contacts{
+		ID:        uuid.NewString(),
+		AccountID: uuid.NewString(),
+		Name:      faker.Name(),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[add contact]: ", contactResp)
+
+	adminContactResp, err := cli.AddAdminContact(&Contacts{
+		ID:        uuid.NewString(),
+		AccountID: uuid.NewString(),
+		Name:      faker.Name(),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[add admin contact]: ", adminContactResp)
+
+	modifyContactResp, err := cli.ModifyContact(&Contacts{
+		ID:          contactResp.Data.Id,
+		AccountID:   contactResp.Data.AccountId,
+		Name:        faker.Name(),
+		PhoneNumber: faker.E164PhoneNumber(),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[modify contact]: ", modifyContactResp)
+
+	contactResp1, err := cli.GetContactByContactId(modifyContactResp.Data.Id)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[get contact]: ", contactResp1)
+	contactResp2, err := cli.GetContactByAccountId(modifyContactResp.Data.AccountId)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[get contact]: ", contactResp2)
+
+	deleteContact, err := cli.DeleteContact(contactResp.Data.Id)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("[delete contact]: ", deleteContact)
+
+}
