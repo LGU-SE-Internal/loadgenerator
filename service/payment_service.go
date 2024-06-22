ac4c2cf7712fef6881e98be244c6b6f3584af729
liming <1084611818@qq.com>
2024-06-23 00:42:48 +0800

    完成payment-service

--- service/payment_service.go
+++ service/payment_service.go
@@ -1,1 +1,60 @@
 package service
+
+import (
+	"encoding/json"
+	"io"
+)
+
+func (s *SvcImpl) ReqPay(input *Payment) (*PaymentResponse, error) {
+	resp, err := s.cli.SendRequest("POST", s.BaseUrl+"/api/v1/paymentservice/payment", input)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result PaymentResponse
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (s *SvcImpl) ReqAddMoney(input *Payment) (*PaymentResponse, error) {
+	resp, err := s.cli.SendRequest("POST", s.BaseUrl+"/api/v1/paymentservice/payment/money", input)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result PaymentResponse
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (s *SvcImpl) ReqQueryPayment() (*PaymentArrResponse, error) {
+	resp, err := s.cli.SendRequest("GET", s.BaseUrl+"/api/v1/paymentservice/payment", nil)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result PaymentArrResponse
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
--- service/payment_service_test.go
+++ service/payment_service_test.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSvcImpl_ReqQueryPayment(t *testing.T) {
+	cli, _ := GetAdminClient()
+	GetResp, _ := cli.ReqQueryPayment()
+	fmt.Println(GetResp.Msg)
+}
+
+func TestSvcImpl_ReqPay(t *testing.T) {
+	cli, _ := GetAdminClient()
+	UpdateResp, err := cli.ReqPay(&Payment{
+		Id:      "7a9b4f0a-8105-4fa0-b4ca-781d477eea0e",
+		OrderId: "683479c1-757a-4a2c-8dfb-28ab474ee7ad",
+		Price:   "19",
+		UserId:  "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(UpdateResp.Msg)
+}
+
+func TestSvcImpl_ReqAddMoney(t *testing.T) {
+	cli, _ := GetAdminClient()
+	UpdateResp, err := cli.ReqPay(&Payment{
+		Id:      "7a9b4f0a-8105-4fa0-b4ca-781d477eea0e",
+		OrderId: "c157ad12-9f53-466c-9d54-924d547ad224",
+		Price:   "21",
+		UserId:  "1084611818gg",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(UpdateResp.Msg)
+}
