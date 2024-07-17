336d7df26770c727c5336cfce624574f94da0d7e
moumouta <121090617@link.cuhk.edu.cn>
2024-07-18 00:50:07 +0800

    consignprice service, stationfood service, trainfood servcie done.

--- service/station_food_service_test.go
+++ service/station_food_service_test.go
@@ -4,31 +4,43 @@ import "testing"
 
 func TestSvcImpl_GetAllStationFood(t *testing.T) {
 	cli, _ := GetAdminClient()
+	var stationFoodSvc StationFoodService = cli
 
-	resp, err := cli.GetAllStationFood()
+	resp, err := stationFoodSvc.GetAllStationFood()
 	if err != nil {
 		t.Error(err)
+	}
+	if resp.Status != 1 {
+		t.Errorf("GetAllStationFood status should be 1, but is %d", resp.Status)
 	}
 	t.Log(resp)
 
 	for _, stationFood := range resp.Data {
-		sf, err := cli.GetStationFoodByNames([]string{stationFood.StationName})
+		sf, err := stationFoodSvc.GetStationFoodByNames([]string{stationFood.StationName})
 		if err != nil {
 			t.Error(err)
+		}
+		if sf.Status != 1 {
+			t.Errorf("GetStationFoodByNames status should be 1, but is %d", sf.Status)
 		}
 		t.Log(sf)
 
-		sf1, err := cli.GetStationFoodByName(stationFood.StationName)
+		sf1, err := stationFoodSvc.GetStationFoodByName(stationFood.StationName)
 		if err != nil {
 			t.Error(err)
+		}
+		if sf1.Status != 1 {
+			t.Errorf("GetStationFoodByName status should be 1, but is %d", sf1.Status)
 		}
 		t.Log(sf1)
 
-		sf2, err := cli.GetStationFoodById(stationFood.Id)
+		sf2, err := stationFoodSvc.GetStationFoodById(stationFood.Id)
 		if err != nil {
 			t.Error(err)
 		}
-
+		if sf2.Status != 1 {
+			t.Errorf("GetStationFoodById status should be 1, but is %d", sf2.Status)
+		}
 		t.Log(sf2)
 	}
 
