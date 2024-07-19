329bb3ecadf035175daf824638963b404fdfd012
lincyaw <814750204@qq.com>
2024-07-19 14:22:09 +0800

    adopt seat num = int

--- service/test_utils.go
+++ service/test_utils.go
@@ -83,7 +83,7 @@ func NewSvcClients() *SvcImpl {
 	cli.AddHeader("Content-Type", "application/json")
 	cli.AddHeader("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
 	cli.AddHeader("Connection", "keep-alive")
-	baseUrl := "http://10.10.10.220:30437"
+	baseUrl := os.Getenv("BASE_URL")
 	if baseUrl == "" {
 		panic("PLEASE use BASE_URL environment variable, example: BASE_URL=http://127.0.0.1:8080")
 	}
