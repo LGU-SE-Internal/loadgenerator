f493f161ff9ee6d1c83b544304fbb197b209ac58
moumouta <121090617@link.cuhk.edu.cn>
2024-07-21 01:18:12 +0800

    Finsih the modification of consignservcie and contactservice; Fix some base service bugs.

--- service/contacts_service_test.go
+++ service/contacts_service_test.go
@@ -7,9 +7,11 @@ import (
 	"testing"
 )
 
-func TestSvc(t *testing.T) {
-	cli, _ := GetBasicClient()
+func TestSvc_FullIntegration(t *testing.T) {
+	cli, _ := GetAdminClient()
 	var contactsSvc ContactsService = cli
+
+	// CreateContact
 	CreateContactsInput := AdminContacts{
 		Id:        uuid.NewString(),
 		AccountId: uuid.NewString(),
@@ -19,41 +21,81 @@ func TestSvc(t *testing.T) {
 	if err != nil {
 		log.Fatalf("[MockedContactsID] CreateContacts error occurs: %v", err)
 	}
-	if CreateContacts.Data.Id == "" {
-		log.Fatalf("Create AdminContacts Fail. Return Id = ''")
+	if CreateContacts.Status != 1 {
+		t.Errorf("CreateContacts.Status != 1")
 	}
-}
-func TestSvcImpl_GetAllContacts(t *testing.T) {
-	cli, _ := GetBasicClient()
-	contacts, err := cli.GetAllContacts()
+	if CreateContacts.Data.Id == "" {
+		t.Errorf("Create AdminContacts Fail. Return Id = ''")
+	}
+	isMatch := false
+	if /*CreateContacts.Data.Id == CreateContactsInput.Id &&*/
+	CreateContacts.Data.Name == CreateContactsInput.Name &&
+		CreateContacts.Data.AccountId == CreateContactsInput.AccountId &&
+		CreateContacts.Data.PhoneNumber == CreateContactsInput.PhoneNumber &&
+		CreateContacts.Data.DocumentNumber == CreateContactsInput.DocumentNumber &&
+		CreateContacts.Data.DocumentType == CreateContactsInput.DocumentType {
+		isMatch = true
+	}
+	if !isMatch {
+		t.Errorf("Create AdminContacts Fail. expect: %v, get %v", CreateContactsInput, CreateContacts.Data)
+	}
+	existedContacts := CreateContacts.Data
+
+	// getAllContacts
+	contacts, err := contactsSvc.GetAllContacts()
 	if err != nil {
 		t.Error(err)
+	}
+	if contacts.Status != 1 {
+		t.Errorf("contacts.Status != 1")
+	}
+	isMatch1 := false
+	for _, contact := range contacts.Data {
+		if contact.Id == existedContacts.Id &&
+			contact.Name == existedContacts.Name &&
+			contact.AccountId == existedContacts.AccountId &&
+			contact.PhoneNumber == existedContacts.PhoneNumber &&
+			contact.DocumentNumber == existedContacts.DocumentNumber &&
+			contact.DocumentType == existedContacts.DocumentType {
+			isMatch1 = true
+		}
+	}
+	if !isMatch1 {
+		t.Errorf("Except to find: %v, but failed.", existedContacts)
 	}
 	t.Log("[get all]: ", contacts)
 
-	contactResp, err := cli.AddContact(&AdminContacts{
+	// AddAdminContact
+	adminContactReq := AdminContacts{
 		Id:        uuid.NewString(),
 		AccountId: uuid.NewString(),
 		Name:      faker.Name(),
-	})
+	}
+	adminContactResp, err := cli.AddAdminContact(&adminContactReq)
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log("[add contact]: ", contactResp)
-
-	adminContactResp, err := cli.AddAdminContact(&AdminContacts{
-		Id:        uuid.NewString(),
-		AccountId: uuid.NewString(),
-		Name:      faker.Name(),
-	})
-	if err != nil {
-		t.Error(err)
+	if adminContactResp.Status != 1 {
+		t.Errorf("adminContactResp.Status != 1")
+	}
+	isMatch2 := false
+	if /*adminContactResp.Data.Id == adminContactReq.Id &&*/
+	adminContactResp.Data.Name == adminContactReq.Name &&
+		adminContactResp.Data.AccountId == adminContactReq.AccountId &&
+		adminContactResp.Data.PhoneNumber == adminContactReq.PhoneNumber &&
+		adminContactResp.Data.DocumentNumber == adminContactReq.DocumentNumber &&
+		adminContactResp.Data.DocumentType == adminContactReq.DocumentType {
+		isMatch2 = true
+	}
+	if !isMatch2 {
+		t.Errorf("Do not macth. expect: %v, get %v", adminContactReq, adminContactResp)
 	}
 	t.Log("[add admin contact]: ", adminContactResp)
 
+	// Modify Contacts
 	modifyContactResp, err := cli.ModifyContact(&AdminContacts{
-		Id:          contactResp.Data.Id,
-		AccountId:   contactResp.Data.AccountId,
+		Id:          existedContacts.Id,
+		AccountId:   existedContacts.AccountId,
 		Name:        faker.Name(),
 		PhoneNumber: faker.E164PhoneNumber(),
 	})
@@ -62,20 +104,55 @@ func TestSvcImpl_GetAllContacts(t *testing.T) {
 	}
 	t.Log("[modify contact]: ", modifyContactResp)
 
-	contactResp1, err := cli.GetContactByContactId(modifyContactResp.Data.Id)
+	contactResp1, err := cli.GetContactByContactId(existedContacts.Id)
 	if err != nil {
 		t.Error(err)
 	}
+	if contactResp1.Status != 1 {
+		t.Errorf("contactResp1.Status != 1")
+	}
+	found := false
+	if /*contactResp1.Data.Id == existedContacts.Id &&*/
+	contactResp1.Data.AccountId == existedContacts.AccountId &&
+		/*contactResp1.Data.PhoneNumber == existedContacts.PhoneNumber &&*/
+		contactResp1.Data.DocumentNumber == existedContacts.DocumentNumber &&
+		contactResp1.Data.DocumentType == existedContacts.DocumentType {
+		found = true
+	}
+	if !found {
+		t.Errorf("Except to find: %v, but failed.", existedContacts)
+	}
 	t.Log("[get contact]: ", contactResp1)
-	contactResp2, err := cli.GetContactByAccountId(modifyContactResp.Data.AccountId)
+
+	contactResp2, err := cli.GetContactByAccountId(existedContacts.AccountId)
 	if err != nil {
 		t.Error(err)
+	}
+	if contactResp2.Status != 1 {
+		t.Errorf("contactResp2.Status != 1")
+	}
+	found1 := false
+	for _, contact := range contactResp2.Data {
+		if contact.Id == existedContacts.Id &&
+			/*contact.Name == existedContacts.Name &&*/
+			contact.AccountId == existedContacts.AccountId &&
+			/*contact.PhoneNumber == existedContacts.PhoneNumber &&*/
+			contact.DocumentType == existedContacts.DocumentType &&
+			contact.DocumentNumber == existedContacts.DocumentNumber {
+			found1 = true
+		}
+	}
+	if !found1 {
+		t.Errorf("Except to find: %v, but failed.", existedContacts)
 	}
 	t.Log("[get contact]: ", contactResp2)
 
-	deleteContact, err := cli.DeleteContact(contactResp.Data.Id)
+	deleteContact, err := cli.DeleteContact(existedContacts.Id)
 	if err != nil {
 		t.Error(err)
+	}
+	if deleteContact.Status != 1 {
+		t.Errorf("deleteContact.Status != 1")
 	}
 	t.Log("[delete contact]: ", deleteContact)
 
