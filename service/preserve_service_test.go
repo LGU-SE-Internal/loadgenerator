d60daa09dbcad0faeb550b60b2fe0675fde088e6
moumouta <121090617@link.cuhk.edu.cn>
2024-07-28 04:34:54 +0800

    Modify the contacts_service_test

--- service/preserve_service_test.go
+++ service/preserve_service_test.go
@@ -11,20 +11,7 @@ func TestSvcImpl_Preserve(t *testing.T) {
 	cli, _ := GetBasicClient()
 	var preserveSvc PreserveService = cli
 
-	/*	var contactsSvc ContactsService = cli
-		var getContactsId string
-
-		GetAllContactsId, err := contactsSvc.GetAllContacts()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Log(GetAllContactsId)
-
-		if len(GetAllContactsId.Data) > 0 {
-			//getContactsId = *(GetAllContactsId.Data[0].AccountId)
-			getContactsId = GetAllContactsId.Data[0].Id
-		}*/
-
+	// LoginToken
 	loginResult, err := cli.ReqUserLogin(&UserLoginInfoReq{
 		Password:         "111111",
 		UserName:         "fdse_microservice",
@@ -34,10 +21,143 @@ func TestSvcImpl_Preserve(t *testing.T) {
 		log.Fatalln(err)
 	}
 
-	MockedAccountID := /*faker.UUIDHyphenated()*/ "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"
-	MockedContactsID := /*getContactsId*/ "95e046ea-812c-49a9-8d12-168f539bec43"
-	MockedTripID := /*faker.UUIDHyphenated()*/ "92708982-77af-4318-be25-57ccb0ff69ad"
-	MockedLoginToken := /*faker.UUIDHyphenated()*/ loginResult.Data.Token
+	// Security Service
+	// Mock Account ID
+	var securitySvc SecurityService = cli
+
+	//MockedID := faker.UUIDHyphenated()
+	MockedName := faker.Name()
+	MockedValue := generateRandomNumberString()
+	MockedDescription := generateDescription()
+
+	// Mock input
+	input := &SecurityConfig{
+		//ID:          MockedID,
+		Name:        MockedName,
+		Value:       MockedValue,
+		Description: MockedDescription,
+	}
+
+	// Add Security AdminConfig
+	addResp, err := securitySvc.AddNewSecurityConfig(input)
+	if err != nil {
+		t.Errorf("AddNewSecurityConfig failed: %v", err)
+	}
+	if addResp.Status != 1 {
+		t.Errorf("[Security Service]addResp.Status != 1")
+	}
+	if addResp.Msg == "Already exists" {
+		t.Logf("[Security Service]addResp.Msg => Already exists")
+		t.Skip()
+	}
+	isMatch := false
+	if /*addResp.Data.ID == input.ID &&*/
+	addResp.Data.Value == input.Value &&
+		addResp.Data.Description == input.Description &&
+		addResp.Data.Name == input.Name {
+		isMatch = true
+	}
+	if !isMatch {
+		t.Errorf("[Security Service]Except: %v, get %v", input, addResp.Data)
+	}
+	t.Logf("AddNewSecurityConfig response: %+v", addResp)
+	existedSecurity := addResp.Data
+
+	// Contact Service
+	// Mock Contacts ID
+	var contactsSvc ContactsService = cli
+
+	// CreateContact
+	CreateContactsInput := AdminContacts{
+		Id:        faker.UUIDHyphenated(),
+		AccountId: existedSecurity.ID,
+		Name:      faker.Name(),
+	}
+	CreateContacts, err := contactsSvc.AddContact(&CreateContactsInput)
+	if err != nil {
+		log.Fatalf("[MockedContactsID] CreateContacts error occurs: %v", err)
+	}
+	if CreateContacts.Status != 1 {
+		t.Errorf("CreateContacts.Status != 1")
+	}
+	if CreateContacts.Data.Id == "" {
+		t.Errorf("Create AdminContacts Fail. Return Id = ''")
+	}
+	isMatch1 := false
+	if /*CreateContacts.Data.Id == CreateContactsInput.Id &&*/
+	CreateContacts.Data.Name == CreateContactsInput.Name &&
+		CreateContacts.Data.AccountId == CreateContactsInput.AccountId &&
+		CreateContacts.Data.PhoneNumber == CreateContactsInput.PhoneNumber &&
+		CreateContacts.Data.DocumentNumber == CreateContactsInput.DocumentNumber &&
+		CreateContacts.Data.DocumentType == CreateContactsInput.DocumentType {
+		isMatch1 = true
+	}
+	if !isMatch1 {
+		t.Errorf("Create AdminContacts Fail. expect: %v, get %v", CreateContactsInput, CreateContacts.Data)
+	}
+	existedContacts := CreateContacts.Data
+
+	// Travel Service
+	var travelSvc TravelService = cli
+	// Mock Trip
+	// Mock para
+	MockedLoginId := loginResult.Data.Token
+	MockedTrainTypeName := GenerateTrainTypeName() /*"GaoTieSeven"*/
+	MockedRouteID := faker.UUIDHyphenated()
+	MockedStartStationName := faker.GetRealAddress().City
+	MockedStationsName := faker.GetRealAddress().City
+	MockedTerminalStationName := faker.GetRealAddress().City
+	MockedStartTime := /*getRandomTime()*/ "2099-05-04 15:51:52"
+	MockedEndTime := /*getRandomTime()*/ "2099-07-07 15:51:52"
+	MockedTripId := GenerateTripId()
+
+	// Mock input
+	travelInfo := TravelInfo{
+		LoginID:             MockedLoginId,
+		TripID:              MockedTripId,
+		TrainTypeName:       MockedTrainTypeName,
+		RouteID:             MockedRouteID,
+		StartStationName:    MockedStartStationName,
+		StationsName:        MockedStationsName,
+		TerminalStationName: MockedTerminalStationName,
+		StartTime:           MockedStartTime,
+		EndTime:             MockedEndTime,
+	}
+
+	// Create Test
+	createResp, err := travelSvc.CreateTrip(&travelInfo)
+	if err != nil {
+		t.Errorf("CreateTrip request failed, err %s", err)
+	}
+	if createResp.Status != 1 {
+		t.Errorf("CreateTrip failed: %s", createResp.Msg)
+	}
+	if createResp.Msg != "Already exists" {
+		t.Logf("Already exists: %s", createResp.Msg)
+		t.Skip()
+	}
+	isMatch2 := false
+	if /*createResp.Data.Id == travelInfo.LoginID &&*/
+	createResp.Data.StationsName == toLowerCaseAndRemoveSpaces(travelInfo.StationsName) &&
+		createResp.Data.StartStationName == toLowerCaseAndRemoveSpaces(travelInfo.StartStationName) &&
+		createResp.Data.TerminalStationName == toLowerCaseAndRemoveSpaces(travelInfo.TerminalStationName) &&
+		createResp.Data.StartTime == travelInfo.StartTime &&
+		createResp.Data.EndTime == travelInfo.EndTime &&
+		createResp.Data.TrainTypeName == travelInfo.TrainTypeName &&
+		createResp.Data.RouteId == travelInfo.RouteID {
+		isMatch2 = true
+	}
+	if !isMatch2 {
+		t.Errorf("CreateTrip failed: %s. Except: %v, but get: %v", createResp.Msg, travelInfo, createResp.Data)
+	}
+	existedTravel := createResp.Data
+
+	// Mock Data End
+	// Data Input
+	MockedLoginToken := loginResult.Data.Token
+	MockedAccountID := existedSecurity.ID
+	MockedContactsID := existedContacts.Id
+	MockedTripID := existedTravel.Id
 	MockedDate := /*faker.Date()*/ "2025-05-04 09:00:00"
 	MockedFromCity := /*faker.GetRealAddress().City*/ "suzhou"
 	MockedToCity := /*faker.GetRealAddress().City*/ "beijing"
