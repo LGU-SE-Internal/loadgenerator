3f1632eb754e21cd789f80d234e6f1a0fa2fd237
moumouta <121090617@link.cuhk.edu.cn>
2024-08-02 15:24:50 +0800

    Finished the basic structure construction of preserveBehaviors.

--- behaviors/preserve.go
+++ behaviors/preserve.go
@@ -3,44 +3,77 @@ package behaviors
 import (
 	"fmt"
 	"github.com/Lincyaw/loadgenerator/service"
+	"time"
 )
 
-const ()
+const (
+	AccountID  = "accountId"
+	ContactsID = "contactsId"
+	TripID     = "tripId"
+	SeatType   = "seatType"
+	//LoginToken = "loginToken"
+	Date            = "date"
+	From            = "from"
+	To              = "to"
+	Assurance       = "assurance"
+	FoodType        = "foodType"
+	StationName     = "stationName"
+	StoreName       = "storeName"
+	FoodName        = "foodName"
+	FoodPrice       = "foodPrice"
+	HandleDate      = "handleDate"
+	ConsigneeName   = "consigneeName"
+	ConsigneePhone  = "consigneePhone"
+	ConsigneeWeight = "consigneeWeight"
+	IsWithin        = "isWithin"
+)
 
 var PreserveChain *Chain
 
 func init() {
+	// init
 	PreserveChain = NewChain(NewFuncNode(func(context *Context) (*NodeResult, error) {
+		fmt.Printf("PreserveBehaviors(Chain) Statrs. Starts time: %v", time.Now().String())
 		return nil, nil
 	}))
-	PreserveChain.AddNextChain(NewChain(NewFuncNode(LoginAdmin)), 1)
+
+	// NewFuncNode
+	LoginAdminNode := NewFuncNode(LoginAdmin)
+
+	// NewChain
+	LoginAdminChain := NewChain(LoginAdminNode)
+
+	// AddNextChain
+	PreserveChain.AddNextChain(LoginAdminChain, 1)
+
 }
 
+// Preserve Behaviors
 func Preserve(ctx *Context) (*NodeResult, error) {
 	cli, ok := ctx.Get(Client).(*service.SvcImpl)
 	if !ok {
 		return nil, fmt.Errorf("service client not found in context")
 	}
 	OrderTicketsInfo := service.OrderTicketsInfo{
-		AccountID:       MockedAccountID,
-		ContactsID:      MockedContactsID,
-		TripID:          MockedTripID,
-		SeatType:        MockedSeatType,
+		AccountID:       ctx.Get(AccountID).(string),
+		ContactsID:      ctx.Get(ContactsID).(string),
+		TripID:          ctx.Get(TripID).(string),
+		SeatType:        ctx.Get(SeatType).(int),
 		LoginToken:      ctx.Get(LoginToken).(string),
-		Date:            MockedDate,
-		From:            MockedFromCity,
-		To:              MockedToCity,
-		Assurance:       MockedAssurance,
-		FoodType:        MockedFoodType,
-		StationName:     MockedStationName,
-		StoreName:       MockedStoreName,
-		FoodName:        MockedFoodName,
-		FoodPrice:       MockedFoodPrice,
-		HandleDate:      MockedHandleDate,
-		ConsigneeName:   MockedConsigneeName,
-		ConsigneePhone:  MockedConsigneePhone,
-		ConsigneeWeight: MockedConsigneeWeight,
-		IsWithin:        MockedIsWithin,
+		Date:            ctx.Get(Date).(string),
+		From:            ctx.Get(From).(string),
+		To:              ctx.Get(To).(string),
+		Assurance:       ctx.Get(Assurance).(int),
+		FoodType:        ctx.Get(FoodType).(int),
+		StationName:     ctx.Get(StationName).(string),
+		StoreName:       ctx.Get(StoreName).(string),
+		FoodName:        ctx.Get(FoodName).(string),
+		FoodPrice:       ctx.Get(FoodPrice).(float64),
+		HandleDate:      ctx.Get(HandleDate).(string),
+		ConsigneeName:   ctx.Get(ConsigneeName).(string),
+		ConsigneePhone:  ctx.Get(ConsigneePhone).(string),
+		ConsigneeWeight: ctx.Get(ConsigneeWeight).(float64),
+		IsWithin:        ctx.Get(IsWithin).(bool),
 	}
 	PreserveResp, err := cli.Preserve(&OrderTicketsInfo)
 	if err != nil {
@@ -50,6 +83,7 @@ func Preserve(ctx *Context) (*NodeResult, error) {
 		return nil, fmt.Errorf("preserve order tickets fail. PreserveResp.Status != 1, get %v", PreserveResp.Status)
 	}
 	fmt.Printf("The Status is: %v, and PreserveResp Data: %v\n", PreserveResp.Status, PreserveResp.Data)
+	fmt.Printf("PreserveBehaviors(Chain) Ends. End time: %v", time.Now().String())
 
 	//return nil, err
 	return &(NodeResult{false}), nil
