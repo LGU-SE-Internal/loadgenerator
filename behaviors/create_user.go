e3b20ff5ed47f129bc05f63776ab10503d59af7a
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-08-02 13:46:42 +0800

    Modify the travel_service_test

--- behaviors/create_user.go
+++ behaviors/create_user.go
@@ -9,9 +9,10 @@ import (
 )
 
 const (
-	UserName = "username"
-	Password = "password"
-	UserId   = "userid"
+	UserName   = "username"
+	Password   = "password"
+	UserId     = "userid"
+	LoginToken = "loginToken"
 )
 
 var LoginChain *Chain
@@ -29,12 +30,16 @@ func LoginAdmin(ctx *Context) (*NodeResult, error) {
 		return nil, fmt.Errorf("service client not found in context")
 	}
 	// login
-	_, err := cli.ReqUserLogin(&service.UserLoginInfoReq{
+	loginResult, err := cli.ReqUserLogin(&service.UserLoginInfoReq{
 		Password:         "222222",
 		UserName:         "admin",
 		VerificationCode: "123",
 	})
-	return nil, err
+	if err != nil {
+		return nil, err
+	}
+	ctx.Set(LoginToken, loginResult.Data.Token)
+	return nil, nil
 }
 
 func LoginNormal(ctx *Context) (*NodeResult, error) {
