f38fa2244e520226886e6351565aefd535162a21
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-09-01 14:36:51 +0800

    orderCancel Behavior Done

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	composedChain := behaviors.NewChain(behaviors.NewFuncNode(func(ctx *behaviors.Context) (*behaviors.NodeResult, error) {
 		return nil, nil
 	}, "dummy"))
-	composedChain.AddNextChain(behaviors.OrderConsignChain, 0.5)
+	composedChain.AddNextChain(behaviors.OrderCancelChain, 0.5)
 	//composedChain.AddNextChain(behaviors.NormalOrderPayChain, 0.5)
 	lg.Start(behaviors.WithThread(1), behaviors.WithSleep(1000), behaviors.WithChain(composedChain))
 }
