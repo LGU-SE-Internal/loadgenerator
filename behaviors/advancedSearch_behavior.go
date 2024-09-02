e883ce8fabe6f00b469b1d10b0f8130e954e5301
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-09-03 01:02:29 +0800

    travelAndTravel2 Modified & orderChange Behavior Done.

--- behaviors/advancedSearch_behavior.go
+++ behaviors/advancedSearch_behavior.go
@@ -1,1 +1,14 @@
 package behaviors
+
+const ()
+
+var AdvancedSearchChain *Chain
+
+func init() {
+	AdvancedSearchChain = NewChain(
+		NewFuncNode(VerifyCode, "VerifyCode"),
+		NewFuncNode(LoginBasic, "LoginBasic"),
+		NewFuncNode(QueryUser, "QueryUser"),
+		NewFuncNode(TravelPlan)
+	)
+}
