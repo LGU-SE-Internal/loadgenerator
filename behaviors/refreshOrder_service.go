3a74fc4e3cc44ae1b84479159bd89d12ace30c62
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-09-03 05:44:14 +0800

    ticketCollectAndEnterStation Behavior Done!

--- behaviors/refreshOrder_service.go
+++ behaviors/refreshOrder_service.go
@@ -189,3 +189,92 @@ func RefreshOrderOther(ctx *Context) (*NodeResult, error) {
 
 	return nil, nil
 }
+
+func RefreshCollectedOrder(ctx *Context) (*NodeResult, error) {
+	cli, ok := ctx.Get(Client).(*service.SvcImpl)
+	if !ok {
+		return nil, fmt.Errorf("service client not found in context")
+	}
+
+	orderInfo := service.OrderInfo{
+		BoughtDateEnd:         "",
+		BoughtDateStart:       "",
+		EnableBoughtDateQuery: false,
+		EnableStateQuery:      false,
+		EnableTravelDateQuery: false,
+		LoginId:               ctx.Get(AccountID).(string),
+		State:                 1,
+		TravelDateEnd:         "",
+		TravelDateStart:       "",
+	}
+
+	var orderSvc service.OrderService = cli
+	RefreshResp, err := orderSvc.ReqQueryOrderForRefresh(&orderInfo)
+	if err != nil {
+		log.Errorf("Refresh Order Request failed, err %s", err)
+		return nil, err
+	}
+	if RefreshResp.Status != 1 {
+		log.Errorf("RefreshResp.Status != 1")
+		return nil, err
+	}
+
+	orderOtherInfo := service.OrderInfo{
+		BoughtDateEnd:         "",
+		BoughtDateStart:       "",
+		EnableBoughtDateQuery: false,
+		EnableStateQuery:      false,
+		EnableTravelDateQuery: false,
+		LoginId:               ctx.Get(AccountID).(string),
+		State:                 1,
+		TravelDateEnd:         "",
+		TravelDateStart:       "",
+	}
+
+	var orderOtherSvc service.OrderOtherService = cli
+	RefreshOtherResp, errOther := orderOtherSvc.ReqQueryOrderForRefreshOther(&orderOtherInfo)
+	if errOther != nil {
+		log.Errorf("Refresh Other Order Request failed, errOther %s", errOther)
+		return nil, errOther
+	}
+	if RefreshOtherResp.Status != 1 {
+		log.Errorf("RefreshOtherResp.Status != 1")
+		return nil, errOther
+	}
+
+	var randomIndex int
+	if len(RefreshResp.Data) == 0 {
+		log.Errorf("Unpaied order is empty")
+		return &NodeResult{Continue: false}, nil
+	}
+	//if rand.Intn(2) == 0 {
+	randomIndex = rand.Intn(len(RefreshResp.Data))
+	ctx.Set(TrainNumber, RefreshResp.Data[randomIndex].TrainNumber) // OldTripId
+	ctx.Set(OldTripID, RefreshResp.Data[randomIndex].TrainNumber)   // OldTripId
+	ctx.Set(Price, RefreshResp.Data[randomIndex].Price)
+	ctx.Set(OrderId, RefreshResp.Data[randomIndex].Id) // ID here is exactly the OrderId
+	ctx.Set(From, RefreshResp.Data[randomIndex].From)
+	ctx.Set(To, RefreshResp.Data[randomIndex].To)
+
+	TheBoughtDate, err := time.Parse("2006-01-02 15:04:05", RefreshResp.Data[randomIndex].BoughtDate)
+	if err != nil {
+		log.Errorf("TheBoughtDate Transformation is failed, err %s", err)
+	}
+	formattedBoughtDate := TheBoughtDate.Format("2006-01-02")
+	ctx.Set(HandleDate, formattedBoughtDate)
+
+	TheTravelDate, err := time.Parse("2006-01-02", RefreshResp.Data[randomIndex].TravelDate)
+	if err != nil {
+		log.Errorf("TheTravelDate Transformation is failed, err %s", err)
+	}
+	formattedTravelDate := TheTravelDate.Format("2006-01-02")
+	ctx.Set(TargetDate, formattedTravelDate)
+	//} else {
+	//	randomIndex = rand.Intn(len(RefreshOtherResp.Data))
+	//	ctx.Set(TrainNumber, RefreshOtherResp.Data[randomIndex].TrainNumber)
+	//	ctx.Set(Price, RefreshOtherResp.Data[randomIndex].Price)
+	//	ctx.Set(OrderId, RefreshOtherResp.Data[randomIndex].Id)
+	//}
+
+	return nil, nil
+}
--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 			composedChain := behaviors.NewChain(behaviors.NewFuncNode(func(ctx *behaviors.Context) (*behaviors.NodeResult, error) {
 				return nil, nil
 			}, "dummy"))
-			composedChain.AddNextChain(behaviors.TicketCollectChain, 0.5)
+			composedChain.AddNextChain(behaviors.TicketCollectAndEnterStationChain, 0.5)
 			lg.Start(behaviors.WithThread(1), behaviors.WithSleep(1000), behaviors.WithChain(composedChain))
 		},
 	}
