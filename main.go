59fcb283f5a4e7f4e1ff064c17c1fbb78c5c5d00
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2024-12-22 21:51:23 +0800

    allow passing `threads` and `sleep` parameters
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Lincyaw/loadgenerator/behaviors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 func main() {
 	// 定义命令行选项变量
 	var debug bool
+	var threads int
+	var sleepDuration int
 
 	// 创建一个新的cobra命令
 	var rootCmd = &cobra.Command{
@@ -40,12 +43,14 @@ func main() {
 			composedChain.AddNextChain(behaviors.ConsignListChain, 8)
 			composedChain.AddNextChain(behaviors.OrderChangeChain, 3)
 			composedChain.AddNextChain(behaviors.OrderCancelChain, 2)
-			lg = behaviors.NewLoadGenerator(behaviors.WithThread(3), behaviors.WithSleep(100), behaviors.WithChain(composedChain))
+			lg = behaviors.NewLoadGenerator(behaviors.WithThread(threads), behaviors.WithSleep(sleepDuration), behaviors.WithChain(composedChain))
 			lg.Start()
 		},
 	}
 
 	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "Enable debug logging")
+	rootCmd.PersistentFlags().IntVarP(&threads, "threads", "t", 3, "Number of threads")
+	rootCmd.PersistentFlags().IntVarP(&sleepDuration, "sleep", "s", 100, "Sleep duration in milliseconds")
 
 	if err := rootCmd.Execute(); err != nil {
 		logrus.Fatalf("Error executing command: %v", err)
