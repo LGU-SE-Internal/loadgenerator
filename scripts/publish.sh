d1e5f07be608282bc54dabf536b64b1c6d44b9d4
rainystevn1 <rainystevn1@gmail.com>
2026-02-10 10:53:28 +0800

    add release manually

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,16 +2,18 @@
 set -e
 
 CHART_DIR="charts/loadgenerator"
-REPO_NAME="loadgenerator"
-REPO_URL="https://lgu-se-internal.github.io/loadgenerator"
+
+ORG_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f1 | tr '[:upper:]' '[:lower:]')
+REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2 | tr '[:upper:]' '[:lower:]')
+REPO_URL="https://${ORG_NAME}.github.io/${REPO_NAME}"
+
+echo "Target Repo URL: $REPO_URL"
 
 helm dependency update $CHART_DIR
 
-# 打包 Chart
 mkdir -p .deploy
 helm package $CHART_DIR -d .deploy
 
-# 生成或更新 index.yaml
 cd .deploy
 if [ -f index.yaml ]; then
     helm repo index . --url $REPO_URL --merge index.yaml
